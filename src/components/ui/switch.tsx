c85c44206b06a4cc82d61df2305ce68904444bb6
ION <thegoshagames@gmail.com>
2024-05-16 12:37:13 +0300

    Изменен элемент уведомлений

--- src/components/ui/switch.tsx
+++ src/components/ui/switch.tsx
@@ -1,12 +1,14 @@
 import { ComponentProps } from 'react'
-import { HStack, Switch, Text } from '@gluestack-ui/themed'
+import { HStack, Switch, Text, VStack } from '@gluestack-ui/themed'
+import { AppColors } from '@/constants/theme'
 
 type SwitchProps = ComponentProps<typeof Switch>
 type Props = {
     label: string
+    description: string
 } & SwitchProps
 
-const AppSwitch = ({ label, ...props }: Props) => (
+const AppSwitch = ({ label, description, ...props }: Props) => (
     <HStack
         justifyContent="space-between"
         alignItems="center"
@@ -16,7 +18,14 @@ const AppSwitch = ({ label, ...props }: Props) => (
             }
         }}
     >
-        <Text>{label}</Text>
+        <VStack flex={1}>
+            <Text fontWeight="$medium" color={AppColors.text} flexShrink={1}>
+                {label}
+            </Text>
+            <Text fontSize="$sm" color={AppColors.hint} flexShrink={1}>
+                {description}
+            </Text>
+        </VStack>
         <Switch aria-label={label} {...props} />
     </HStack>
 )
--- src/screens/main/profile/notifications-screen.tsx
+++ src/screens/main/profile/notifications-screen.tsx
@@ -1,17 +1,36 @@
-import { useState } from 'react'
 import { ScrollView, VStack } from '@gluestack-ui/themed'
 import { useTranslation } from 'react-i18next'
+import { z } from 'zod'
+import { CustomForm, useForm } from '@/components/form/form'
 import TopBar from '@/components/top-bar/top-bar'
+import { FormField, FormItem } from '@/components/ui/form'
 import Scaffold from '@/components/ui/scaffold'
 import AppSwitch from '@/components/ui/switch'
 import { DefaultStackScreenProps } from '@/types/interface'
+
+const notificationsSchema = z.object({
+    n1: z.boolean(),
+    n2: z.boolean(),
+    n3: z.boolean(),
+    n4: z.boolean(),
+    n5: z.boolean(),
+})
 
 export default function NotificationsScreen({
     navigation,
 }: DefaultStackScreenProps) {
     const { t } = useTranslation()
 
-    const [notifications, setNotifications] = useState(true)
+    const form = useForm({
+        schema: notificationsSchema,
+        defaultValues: {
+            n1: true,
+            n2: true,
+            n3: false,
+            n4: false,
+            n5: true,
+        },
+    })
 
     return (
         <Scaffold>
@@ -20,15 +39,80 @@ export default function NotificationsScreen({
                 navigation={navigation}
             />
             <ScrollView>
-                <VStack p="$4" gap="$4">
-                    <AppSwitch
-                        label="Уведомления 1"
-                        value={notifications}
-                        onValueChange={setNotifications}
-                    />
-                    <AppSwitch label="Уведомления 2" />
-                    <AppSwitch label="Уведомления 3" />
-                </VStack>
+                <CustomForm form={form}>
+                    <VStack p="$4" gap="$5">
+                        <FormField
+                            control={form.control}
+                            name="n1"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <AppSwitch
+                                        label="Новости, специальные предложения"
+                                        description="Получайте уведомления об обновлениях и специальных предложениях"
+                                        value={field.value}
+                                        onValueChange={field.onChange}
+                                    />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="n2"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <AppSwitch
+                                        label="Системные уведомления"
+                                        description="Получайте системные уведомления"
+                                        value={field.value}
+                                        onValueChange={field.onChange}
+                                    />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="n3"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <AppSwitch
+                                        label="Уведомления 3"
+                                        description="Информация об уведомлениях номер 3"
+                                        value={field.value}
+                                        onValueChange={field.onChange}
+                                    />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="n4"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <AppSwitch
+                                        label="Уведомления 4"
+                                        description="Информация об уведомлениях номер 4"
+                                        value={field.value}
+                                        onValueChange={field.onChange}
+                                    />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="n5"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <AppSwitch
+                                        label="Уведомления 5"
+                                        description="Информация об уведомлениях номер 5"
+                                        value={field.value}
+                                        onValueChange={field.onChange}
+                                    />
+                                </FormItem>
+                            )}
+                        />
+                    </VStack>
+                </CustomForm>
             </ScrollView>
         </Scaffold>
     )
